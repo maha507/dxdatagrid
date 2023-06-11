57b41ad650f8ef0b7afbe4cb1e17914f5ae4f2c3
maha507 <132257569+maha507@users.noreply.github.com>
2023-06-11 09:27:20 +0530

    a

--- src/App.svelte
+++ src/App.svelte
@@ -84,31 +84,32 @@
 			  console.error("Failed to add record:", error);
 			}
 		  },
-		 		 onRowUpdating: async (e) => {
- 				 console.log("Data sent to API:", e.newData);
- 				 try {
-  					  const response = await fetch(
-     				 `https://api.recruitly.io/api/candidate/${e.key}?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154`,
-      					{
-       					 method: "POST",
-       					 headers: {
-        				  "Content-Type": "application/json",
-       					 },
-      					  body: JSON.stringify(e.newData),
-     				 }
-   				 );
-   			 const responseData = await response.json();
-    		if (response.ok) {
-     		 const updatedItemIndex = gridData.findIndex((item) => item.id === e.key);
-     		 gridData[updatedItemIndex] = responseData;
-     		 dataGrid.refresh();
-    		} else {
-    		  console.error("Failed to update record:", responseData.error);
-   			 }
- 			 } catch (error) {
-    			console.error("Failed to update record:", error);
-  			}
-		},
+		  onRowUpdating: async (e) => {
+	try {
+	  console.log(e.newData);
+	  const response = await fetch(
+		`https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154`,
+		{
+		  method: "POST",
+		  headers: {
+			"Content-Type": "application/json",
+		  },
+		  body: JSON.stringify(e.newData),
+		}
+	  );
+	  const responseData = await response.json();
+	  if (response.ok) {
+		const updatedItemIndex = gridData.findIndex((item) => item.id === e.key);
+		gridData.push(e.newdata);
+		gridData[updatedItemIndex] = e.newData;
+		dataGrid.refresh();
+	  } else {
+		console.error("Failed to update record:", responseData.error);
+	  }
+	} catch (error) {
+	  console.error("Failed to update record:", error);
+	}
+  },
 
 		onRowRemoving: async (e) => {
 			  console.log("Data being sent to API:", e.data);
