7709e1e50cc5e975f340dd6ca1de3bc023ba2c8e
maha507 <132257569+maha507@users.noreply.github.com>
2023-06-11 09:42:04 +0530

    a

--- src/App.svelte
+++ src/App.svelte
@@ -88,7 +88,7 @@
 		try {
 	  console.log(e.newData);
 	  const response = await fetch(
-		`https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154`,
+		`https://api.recruitly.io/api/candidate/${e.data.id}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`,
 		{
 		  method: "POST",
 		  headers: {
