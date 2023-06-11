c905cc638adfeab386047c677796c82466034593
maha507 <132257569+maha507@users.noreply.github.com>
2023-06-11 09:46:26 +0530

    a

--- src/App.svelte
+++ src/App.svelte
@@ -100,8 +100,8 @@
 	  const responseData = await response.json();
 	  if (response.ok) {
 		const updatedItemIndex = gridData.findIndex((item) => item.id === e.key);
-		gridData.push(e.newdata);
-		gridData[updatedItemIndex] = e.Data;
+		gridData[updatedItemIndex] = e.newData;
+		
 		dataGrid.refresh();
 	  } else {
 		console.error("Failed to update record:", responseData.error);
