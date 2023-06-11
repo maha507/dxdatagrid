4be60bef19da6561049c4bbded71a73116bc8e93
maha507 <132257569+maha507@users.noreply.github.com>
2023-06-11 06:02:52 +0530

    a

--- src/App.svelte
+++ src/App.svelte
@@ -97,7 +97,7 @@
   
 		  
 		  onRowUpdating: async (e) => {
- 		 console.log("Data being sent to API:", e.Data);
+ 		 console.log("Data sent to API:", e.newData);
   		try {
    		 const updatedData = {
    	    name: e.newData.name,
@@ -106,7 +106,7 @@
     };
 
     const response = await fetch(
-      `https://api.recruitly.io/api/candidate/${e.key}?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535`,
+      `https://api.recruitly.io/api/candidate?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`,
       {
         method: "POST",
         headers: {
@@ -135,7 +135,7 @@
 			console.log("Data being sent to API:", e.data);
 			try {
 			  const response = await fetch(
-				`https://api.recruitly.io/api/candidate/${e.key}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`,
+				`https://api.recruitly.io/api/candidate?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`,
 				{
 				  method: "DELETE",
 				  headers: {
