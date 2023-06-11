a173d878bd270325a5a4377a4dd3b4dfa9a97df3
maha507 <132257569+maha507@users.noreply.github.com>
2023-06-11 09:03:27 +0530

    a

--- src/App.svelte
+++ src/App.svelte
@@ -58,31 +58,33 @@
 		  pageSize: 20,
 		},
 		onRowInserting: async (e) => {
-		  try {
-			const response = await fetch(
-			  "https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154",
-			  {
-				method: "POST",
-				headers: {
-				  "Content-Type": "application/json",
-				 
-				},
-				body: JSON.stringify(e.data),
-			  }
-			);
+			console.log("Data being sent to API:", e.data);
+			try {
+			  const response = await fetch(
+				"https://api.recruitly.io/api/candidate?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA",
+				{
+				  method: "POST",
+				  headers: {
+					"Content-Type": "application/json",
+			
+				  },
+				  body: JSON.stringify(e.data),
+				}
+			  );
   
-			const responseData = await response.json();
-			if (response.ok) {
-			  e.data.id = responseData.id;
-			  gridData.push(e.data);
-			  dataGrid.refresh();
-			} else {
-			  console.error("Failed to add record:", responseData.error);
+			  const responseData = await response.json();
+			  if (response.ok) {
+				
+				e.data.firstName=responseData.fistName;
+				gridData.push(e.data);
+				dataGrid.refresh();
+			  } else {
+				console.error("Failed to add record:", responseData.error);
+			  }
+			} catch (error) {
+			  console.error("Failed to add record:", error);
 			}
-		  } catch (error) {
-			console.error("Failed to add record:", error);
-		  }
-		},
+		  },
 	   onRowUpdating: async (e) => {
 	try {
 	  console.log(e.newData);
