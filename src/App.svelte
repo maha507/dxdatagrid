d4dd70a5eafa8d009ace36c01ab26d23b9ef6dec
maha507 <132257569+maha507@users.noreply.github.com>
2023-06-11 10:39:51 +0530

    a

--- src/App.svelte
+++ src/App.svelte
@@ -88,7 +88,7 @@
 			try {
 	 		 console.log("Data being sent to API:", e.newData);
 	 		 const response = await fetch(
-					`https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154`,
+					`https://api.recruitly.io/api/candidate/${e.data.id}?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154`,
 						{
 		 				 method: "POST",
 		  				headers: {
@@ -110,7 +110,6 @@
 	 		 console.error("Failed to update record:", error);
 			}
  		 },
-
 
 		onRowRemoving: async (e) => {
 			  console.log("Data being sent to API:", e.data);
